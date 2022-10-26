179ef0b0cb1e39199be58bac4c561c2b4d5063a3
Zhenya <zhenya939393@yandex.ru>
2022-10-27 00:19:30 +0300

    Второй

---
+++ Новая папка/MyProject.java
@@ -0,0 +1,12 @@
+public class Something{
+    int num1;
+    int num2;
+
+    public Something(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+		//new
+    }
+
+    //i updated it
+}
